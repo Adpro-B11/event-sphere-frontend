d4ce174416c7cd25caacd37682b57f67ee95dbfd
Ariqmau <ariqmalik@gmail.com>
2025-05-19 18:48:12 +0700

    feat: add authApi

---
+++ lib/authApi.ts
@@ -0,0 +1,39 @@
+import axios from "axios";
+
+// const API_URL = "http://localhost:8080";
+const API_URL = "http://ec2-52-7-42-79.compute-1.amazonaws.com/";
+
+const axiosInstance = axios.create({
+  baseURL: API_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      console.error("Unauthorized access. Redirecting to login.");
+      localStorage.removeItem("token");
+      localStorage.removeItem("user");
+      window.location.href = "/login";
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance;
